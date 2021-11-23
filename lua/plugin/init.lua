0570d51a127a884c0c035a9561e31d68a5e93193
RunThem <iccy.fun@outlook.com>
2021-11-23 13:48:16 +0800

    1

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -1,11 +1,12 @@
 local packer = require('packer')
+local utils = require('utils')
 
 -- :
 function load(plug, config)
   local package_ok, _ = pcall(require, plug)
   if not package_ok then
     return function()
-
+	utils.info(plug .. "error")
     end
   end
   return require('plugin.config.' .. config).setup()
@@ -41,12 +42,6 @@ packer.startup(
       -- 注释
       use({ 'b3nj5m1n/kommentary', config = load('kommentary', 'kommentary') })
 
-      -- 状态栏
-      -- use({ 'nvim-lualine/lualine.nvim', config = load('lualine', 'lualine') })
-
-      -- 快速移动
-      -- use 'ggandor/lightspeed.nvim'
-
       -- Todo清单
       use({ 'folke/todo-comments.nvim', config = load('todo-comments', 'todo-comments') })
 
@@ -57,36 +52,26 @@ packer.startup(
       use({ 'neovim/nvim-lspconfig' }) -- Collection of configurations for built-in LSP client
       use(
         {
-          'williamboman/nvim-lsp-installer',
-          config = load('nvim-lsp-installer', 'nvim-lsp-installer')
-        }
-      )
-      use({ 'L3MON4D3/LuaSnip' }) -- Snippets plugin
-      use(
-        {
           'hrsh7th/nvim-cmp',
           requires = {
             'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path', 'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/cmp-nvim-lua', 'hrsh7th/cmp-calc', 'LuaSnip'
+            'hrsh7th/cmp-nvim-lua', 'hrsh7th/cmp-calc', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip'
           },
           config = load('cmp', 'nvim-cmp')
         }
       ) -- Autocompletion plugin
 
-      use({ 'saadparwaiz1/cmp_luasnip' }) -- Snippets source for nvim-cmp
-
-      -- use 'HungryJoe/trouble.nvim'
-      -- use 'onsails/lspkind-nvim'
-      -- symbols
-      -- Terminal
-      -- use 'akinsho/nvim-toggleterm.lua'
-      -- 缩进线
-      -- use 'lukas-reineke/indent-blankline.nvim'
+      -- use(
+      --   {
+      --     'williamboman/nvim-lsp-installer',
+      --     config = load('nvim-lsp-installer', 'nvim-lsp-installer')
+      --   }
+      -- )
 
       -- 模糊搜索
-      use({ 'nvim-telescope/telescope.nvim', load('telescope', 'telescope') })
-      use({ 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' })
-      use({ 'kevinhwang91/nvim-hlslens' })
+      -- use({ 'nvim-telescope/telescope.nvim', load('telescope', 'telescope') })
+      -- use({ 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' })
+      -- use({ 'kevinhwang91/nvim-hlslens' })
 
       -- git提示
       -- use 'lewis6991/gitsigns.nvim'
